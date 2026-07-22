2921ec30229861bb51b741bce1da80788efb741c
vmargin <marginvalkenburgh@gmail.com>
2026-07-22 08:02:09 +0000

    Day 15: for loop structute, for each, optional label, break and continue statement

--- chapter2/LoopsLesson.java
+++ chapter2/LoopsLesson.java
@@ -1,5 +1,7 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class LoopsLesson {
     public static void main(String[] args) {
         int a = 1;
@@ -14,6 +16,32 @@ public class LoopsLesson {
             System.out.println("b = " + b--);
             
         }
+
+        for (int c = 0; c < 10; c++) { //for loop, can exist without init, update statement
+            System.out.println("c = " + c);
+            
+        }
+        // nest for loop
+        Scanner q = new Scanner;
+        int limit = q.nextInt();
+        System.out.println("Enter a valid num");
+        for(int x=1; limit; x++) {
+            for(int y=1; y<=x; y++);
+
+        }
+        // for each
+        String[] colors = {"black", "red", "pink","yellow","white", "blue"};
+        for(String color : colors);
+            System.out.println( "Color: " + color);
+
+        ROW: for (int p = 1; p<=10; p++){
+        COL:    for (int l; l<=10; l++)
+                    System.out.println(p*l);
+                System.out.println();
+
+        }
+
+
 
     }
     
